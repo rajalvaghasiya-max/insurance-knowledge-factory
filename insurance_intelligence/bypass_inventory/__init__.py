44937a67163ff1fff54fbc95dad97ce8e08bd5c9
rajalvaghasiya-max <rajal.vaghasiya@gmail.com>
2026-07-27 21:53:57 +0530

    feat(intelligence): add bypass inventory package

---
+++ insurance_intelligence/bypass_inventory/__init__.py
@@ -0,0 +1,9 @@
+from insurance_intelligence.bypass_inventory.classifier import (
+    STAR_COMPREHENSIVE_PILOT_ID,
+    build_default_bypass_inventory,
+)
+
+__all__ = [
+    "STAR_COMPREHENSIVE_PILOT_ID",
+    "build_default_bypass_inventory",
+]
